0a2ba809c17553470501c2ee2914bca8cde54182
Sidharth chauhan <chauhansiddharth71@gmail.com>
2026-04-18 11:52:41 +0530

    add

--- s3/sdk/js/readme.md
+++ s3/sdk/js/readme.md
@@ -4,4 +4,8 @@ package.json:- "type": "module"
 
 # AWS SDK
 
+- https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_code_examples.html
+
 -npm install @aws-sdk/client-s3
+
+- https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
---
+++ s3/sdk/js/uploadObject.js
@@ -0,0 +1,28 @@
+import { S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+import fs from "fs";
+
+
+const s3Client=new S3Client({
+  region: 'us-east-1'
+})
+
+async function uploadObj(){
+  try{
+
+    const data=new PutObjectCommand({
+      Bucket: "my-new-bucket-new-live-sidharth",
+      Key: "file1.txt",
+      Body: fs.createReadStream("../../../hello.txt")
+    });
+
+    const response =await s3Client.send(data);
+
+    console.log(response);
+
+  }catch(error){
+    console.log(error);
+  }
+}
+
+
+uploadObj();
